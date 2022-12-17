21e8cf4695f46dbebe4d739220c1cd4cd172dbb3
Вячеслав Тихонов <sl-tx@yandex.ru>
2022-12-17 17:13:32 +0300

    DepartmentTest

--- src/test/java/ru/skypro/weblibrary/service/DepartmentServiceTest.java
+++ src/test/java/ru/skypro/weblibrary/service/DepartmentServiceTest.java
@@ -1,33 +1,92 @@
 package ru.skypro.weblibrary.service;
 
+import org.jetbrains.annotations.NotNull;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
+import ru.skypro.weblibrary.dto.EmployeeRequest;
+import ru.skypro.weblibrary.entity.Employee;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 @ExtendWith(MockitoExtension.class)
 class DepartmentServiceTest {
-
-
-    @InjectMocks
+    static List<Employee> ret = new ArrayList<Employee>();
+    @Mock
     EmployeeService employeeService;
-    final DepartmentService departmentService = new DepartmentServiceImpl(employeeService);
+    @InjectMocks
+    DepartmentServiceImpl departmentService;
+
+    @BeforeAll
+    static void init(){
+        Employee employee;
+        var letArr = new String[]{"A", "B", "C", "D","E"};
+        var i = 0;
+        for (String let:letArr
+        ) {
+            i++;
+            employee = new Employee(
+                    "FirstName"+let,
+                    "LastName"+let,
+                    "SecondName"+let,
+                    i%2+1,
+                    (i%3+1)*1000
+            );
+            ret.add(employee);
+        }
+    }
 
     @Test
     void getEmployees() {
+        Mockito.when(employeeService.getListEmployee()).thenReturn(ret);
+        var expected = ret.stream().collect(Collectors.groupingBy(Employee::getDepartment));
+        var actual = departmentService.getEmployees();
+        assertEquals(expected,actual);
     }
 
-    @Test
-    void getSalarySum() {
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2})
+    void getSalarySum(Integer id) {
+        Mockito.when(employeeService.getListEmployee()).thenReturn(ret);
+        var expected = ret.stream().filter(e-> e.getDepartment().equals(id))
+                .mapToInt(Employee::getSalary)
+                .sum();
+        var actual = departmentService.getSalarySum(id);
+        assertEquals(expected,actual);
     }
 
-    @Test
-    void getSalaryMin() {
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2})
+    void getSalaryMin(Integer id) {
+        Mockito.when(employeeService.getListEmployee()).thenReturn(ret);
+        var expected = ret.stream().filter(e-> e.getDepartment().equals(id))
+                .mapToInt(Employee::getSalary)
+                .min().orElseThrow();
+        var actual = departmentService.getSalaryMin(id);
+        assertEquals(expected,actual);
     }
 
-    @Test
-    void getSalaryMax() {
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2})
+    void getSalaryMax(Integer id) {
+        Mockito.when(employeeService.getListEmployee()).thenReturn(ret);
+        var expected = ret.stream().filter(e-> e.getDepartment().equals(id))
+                .mapToInt(Employee::getSalary)
+                .max().orElseThrow();
+        var actual = departmentService.getSalaryMax(id);
+        assertEquals(expected,actual);
     }
 }
